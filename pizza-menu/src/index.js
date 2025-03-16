5f5d021bfffeb5563ab26007d02a0d5e5235ed3e
RitikGupta2203 <guptaritik2203@gmail.com>
2025-03-16 16:05:29 -0400

    Pizza UI : React

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 //data
 const pizzaData = [
@@ -47,29 +48,7 @@ const pizzaData = [
   },
 ];
 
-function App() {
-  return (
-    <div>
-      <h1>Hello React</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
-  );
-}
-//Creating a components:
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
-
-//2nd Component:
-
+//ROOT RENDER
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -89,3 +68,117 @@ Your app will still work fine, but you might miss out on warnings about potentia
 In production mode, React.StrictMode has no effect—it’s purely for development debugging.
 
  */
+
+//COMPONENTS:
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+//Creating a structure of the app
+
+//HEADER
+function Header() {
+  //one way of adding styles: not recommded
+  //   const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+
+  const style = {};
+
+  //2ndway to import the css file
+
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+//MENU
+function Menu() {
+  const pizzas = pizzaData;
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+
+      {pizzas.length > 0 ? (
+        //div will cause error : as there can be only one element so we wrap it in <></>fragment else to add a key to react fragment
+        //eg: <React.Fragment></React.Fragment>
+        <>
+          <p>
+            Authentic Italian cuisuine.6 creative dishes to chose from.All from
+            our stone oven, all organic, all delicious.{" "}
+          </p>
+
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We are Still working on our menu. Please Come back Later</p>
+      )}
+    </main>
+  );
+}
+
+//OPTIONS
+
+//----------------------------//Receive props
+function Pizza(props) {
+  console.log(props);
+
+  //if (props.pizzaObj.soldOut) return null;
+  return (
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>
+          {props.pizzaObj.soldOut ? "Sold Out" : props.pizzaObj.price + 3}
+        </span>
+      </div>
+    </li>
+  );
+}
+
+//FOOTER
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order openHour={openHour} closeHour={closeHour} />
+      ) : (
+        <p>
+          we are happy to welcome you between {openHour}:00 and {closeHour}:00
+          hrs
+        </p>
+      )}
+    </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're Open until {props.closeHour}:00. Come visit us or Order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+
+//REACT FRAGMENTS: LETS US GROUP ELEMENTS WITHOUT LEAVING ANY TRACE IN html Tree
